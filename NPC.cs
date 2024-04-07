f71e52306bb17e8c02602b4669deadadaf7e55af
AlgoritmikaUnity <165564216+AlgoritmikaUnity@users.noreply.github.com>
2024-04-07 18:01:14 +0500

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC: MonoBehaviour
+{
+    public float posX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posX = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        posX += 0.001f;
+        transform.position = new Vector3(0, posX, 25);
+    }
+}
